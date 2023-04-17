f6acf97446ece8fcd941aca6a3bb1a975d0e656b
Laurens256 <duinlaurens2@gmail.com>
2023-04-17 20:25:42 +0200

    rename

--- routes/routes.ts
+++ routes/routes.ts
@@ -1,10 +1,13 @@
+import chat from './chat.js';
 import home from './home.js';
 
 const routes = [
 	// { path: '/filters', view: filters, viewName: 'FilterView' },
 	// { path: '/pokemon/:name', view: pokemonDetails, viewName: 'PokemonDetailsView' },
 	// { path: '/pokemon', view: pokemonList, viewName: 'PokemonListView' },
+	{ path: '/chat', view: chat, viewName: 'ChatView' },
 	{ path: '/', view: home, viewName: 'HomeView' },
+
 	// { path: '*', view: error, viewName: 'ErrorView' }
 ];
 
--- src/scripts/socket/message.ts
+++ src/scripts/socket/message.ts
@@ -1,19 +1,21 @@
-const msgContainer = document.querySelector('.msg-container') as HTMLUListElement;
-const msgForm = document.querySelector('form.msg-form') as HTMLFormElement;
-const msgInput = msgForm.querySelector('input') as HTMLInputElement;
+const msgContainer = document.querySelector('.msg-container');
+const msgForm = document.querySelector('form.msg-form');
+const msgInput = msgForm?.querySelector('input');
 
-msgForm.addEventListener('submit', (e) => {
-	e.preventDefault();
+if (msgContainer && msgForm && msgInput) {
+	msgForm.addEventListener('submit', (e) => {
+		e.preventDefault();
 
-	if (msgInput.value) {
-		socket.emit('message', msgInput.value);
-		msgInput.value = '';
-	}
-});
+		if (msgInput.value) {
+			socket.emit('message', msgInput.value);
+			msgInput.value = '';
+		}
+	});
 
-socket.on('message', (msg: string) => {
-	msgContainer.appendChild(
-		Object.assign(document.createElement('li'), { textContent: msg })
-	);
-	msgContainer.scrollTop = msgContainer.scrollHeight;
-});
+	socket.on('message', (msg: string) => {
+		msgContainer.appendChild(
+			Object.assign(document.createElement('li'), { textContent: msg })
+		);
+		msgContainer.scrollTop = msgContainer.scrollHeight;
+	});
+}
